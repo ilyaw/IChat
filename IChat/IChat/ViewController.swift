d2f0ca6f4081aac06d73a13771674095c32c7ab9
ilya rudenko <webilya29@gmail.com>
2021-08-08 23:20:00 +0300

    Add project

---
+++ IChat/IChat/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  IChat
+//
+//  Created by Ilya on 08.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+}
+
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        }
+    }
+}
+
