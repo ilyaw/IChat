3331cd02bc51074b5017b5af959d864b3cfdfc27
ilya rudenko <webilya29@gmail.com>
2021-08-18 19:30:05 +0300

    Add controllers activechatcell

---
+++ IChat/IChat/Controllers/Cell/ActiveChatCell.swift
@@ -0,0 +1,23 @@
+//
+//  ActiveChatCell.swift
+//  IChat
+//
+//  Created by Ilya on 18.08.2021.
+//
+
+import UIKit
+
+class ActiveChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
