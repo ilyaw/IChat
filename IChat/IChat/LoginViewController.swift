19389893a3f4ca6ae3f66c2282f2a50c48ca44cf
ilya rudenko <webilya29@gmail.com>
2021-08-19 19:46:42 +0300

    Add photo view

--- IChat/IChat/LoginViewController.swift
+++ IChat/IChat/LoginViewController.swift
@@ -34,6 +34,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        googleButton.customizeGoogleButton()
         view.backgroundColor = .white
         setupConstraints()
     }
--- IChat/IChat/SetupProfileViewController.swift
+++ IChat/IChat/SetupProfileViewController.swift
@@ -8,22 +8,49 @@
 import UIKit
 
 class SetupProfileViewController: UIViewController {
-
+    
+    let fillImageView = AddPhotoView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupConstraints()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Setup constraints
+extension SetupProfileViewController {
+    private func setupConstraints() {
+        
+        fillImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(fillImageView)
+        
+        NSLayoutConstraint.activate([
+            fillImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            fillImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
+
+// MARK: - SwiftUI
+import SwiftUI
+
+struct SetupProfileVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let setupProfileVC = SetupProfileViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<SetupProfileVCProvider.ContainerView>) -> SetupProfileViewController {
+            return setupProfileVC
+        }
+        
+        func updateUIViewController(_ uiViewController: SetupProfileVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<SetupProfileVCProvider.ContainerView>) {
+            
+        }
     }
-    */
-
 }
