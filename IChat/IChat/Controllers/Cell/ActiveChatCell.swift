65caaa2af7b4e76457eec252b61a77bb0241c813
ilya rudenko <webilya29@gmail.com>
2021-08-31 19:13:56 +0300

    Add active chat cell

--- IChat/IChat/Controllers/Cell/ActiveChatCell.swift
+++ IChat/IChat/Controllers/Cell/ActiveChatCell.swift
@@ -7,17 +7,99 @@
 
 import UIKit
 
-class ActiveChatCell: UITableViewCell {
+protocol SelfConfiguringCell {
+    static var reuseId: String { get }
+    func configure(with chat: MChat)
+}
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class ActiveChatCell: UICollectionViewCell, SelfConfiguringCell {
+    static var reuseId: String = "ActiveChatCell"
+    
+    let friendImageView = UIImageView()
+    let friendName = UILabel(text: "User name", font: .laoSangamMN20())
+    let lastMessage = UILabel(text: "How are you?", font: .laoSangamMN18())
+    let gradientView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        setupConstraints()
+        
+        self.layer.cornerRadius = 4
+        self.clipsToBounds = true
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    
+    func configure(with chat: MChat) {
+        friendImageView.image = UIImage(named: chat.userImageString)
+        friendName.text = chat.username
+        lastMessage.text = chat.lastMessage
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+
+// MARK: - Setup constraints
+extension ActiveChatCell {
+    private func setupConstraints() {
+        friendImageView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        friendName.translatesAutoresizingMaskIntoConstraints = false
+        lastMessage.translatesAutoresizingMaskIntoConstraints = false
+        
+        friendImageView.backgroundColor = .orange
+        gradientView.backgroundColor = .black
+        
+        addSubview(friendImageView)
+        addSubview(gradientView)
+        addSubview(friendName)
+        addSubview(lastMessage)
+        
+        NSLayoutConstraint.activate([
+            friendImageView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            friendImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            friendImageView.heightAnchor.constraint(equalToConstant: 78),
+            friendImageView.widthAnchor.constraint(equalToConstant: 78),
+            
+            gradientView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            gradientView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            gradientView.heightAnchor.constraint(equalToConstant: 78),
+            gradientView.widthAnchor.constraint(equalToConstant: 8),
+            
+            friendName.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            friendName.leftAnchor.constraint(equalTo: friendImageView.rightAnchor, constant: 16),
+            friendName.rightAnchor.constraint(equalTo: gradientView.leftAnchor, constant: 16),
+            
+            lastMessage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
+            lastMessage.leftAnchor.constraint(equalTo: friendImageView.rightAnchor, constant: 16),
+            lastMessage.rightAnchor.constraint(equalTo: gradientView.leftAnchor, constant: 16),
+        ])
+    }
+}
+
+// MARK: - SwiftUI
+
+import SwiftUI
+
+struct ActiveChatProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = MainTabBarController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ActiveChatProvider.ContainerView>) -> MainTabBarController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: ActiveChatProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ActiveChatProvider.ContainerView>) {
+            
+        }
+    }
 }
